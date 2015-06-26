4ae9778097ecb75cade3efa0f0267596580021ca
Jeremy Harris <jeremy.harris@zenosmosis.com>
2015-06-26 10:09:37 -0600

    Removed autorelease references w/ ARC conversion

--- NSString+AESCrypt.m
+++ NSString+AESCrypt.m
@@ -10,22 +10,22 @@
 
 - (NSString *)AES256EncryptWithKey:(NSString *)key
 {
-   NSData *plainData = [self dataUsingEncoding:NSUTF8StringEncoding];
-   NSData *encryptedData = [plainData AES256EncryptWithKey:key];
-   
-   NSString *encryptedString = [encryptedData base64Encoding];
-   
-   return encryptedString;
+    NSData *plainData = [self dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *encryptedData = [plainData AES256EncryptWithKey:key];
+    
+    NSString *encryptedString = [encryptedData base64Encoding];
+    
+    return encryptedString;
 }
 
 - (NSString *)AES256DecryptWithKey:(NSString *)key
 {
-   NSData *encryptedData = [NSData dataWithBase64EncodedString:self];
-   NSData *plainData = [encryptedData AES256DecryptWithKey:key];
-   
-   NSString *plainString = [[NSString alloc] initWithData:plainData encoding:NSUTF8StringEncoding];
-   
-   return [plainString autorelease];
+    NSData *encryptedData = [NSData dataWithBase64EncodedString:self];
+    NSData *plainData = [encryptedData AES256DecryptWithKey:key];
+    
+    NSString *plainString = [[NSString alloc] initWithData:plainData encoding:NSUTF8StringEncoding];
+    
+    return plainString;
 }
 
 @end
